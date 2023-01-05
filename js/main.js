63db4beec10c25ad7642d722de3d877a308181d9
Lorenzo789 <pallozzilorenzo@gmail.com>
2023-01-05 23:05:30 +0100

    feat: add function for different difficulty settings

--- js/main.js
+++ js/main.js
@@ -7,13 +7,37 @@ btnGameStart.addEventListener( 'click', function() {
 
     document.getElementById("btn-grid-game").disabled = true;
     
+    const difficultySelect = document.getElementById("select-difficulty").value;
+
+    if (difficultySelect == "1") {
+        createGame(100, 'square');
+    } else if (difficultySelect == "2") {
+        createGame(60, 'square-medium');
+    } else if (difficultySelect == "3") {
+        createGame(40, 'square-hard');
+    }
+
+})
+
+
+// * FUNCTIONS * \\
+function createNewSquare(classToAdd) {
+    const square = document.createElement('div');
+
+    square.classList.add(classToAdd);
+
+    return square;
+}
+
+function createGame(square, classToAdd) {
+
     const parentGrid = document.getElementById("grid-game");
     
-    parentGrid.classList.add('grid-container')
-    
-    for (let index = 0; index < 100; index++) {
-        
-        const newSquare = createNewSquare();
+    parentGrid.classList.add('grid-container');
+
+    for (let index = 0; index < square; index++) {
+            
+        const newSquare = createNewSquare(classToAdd);
         
         newSquare.innerHTML = (index + 1);
         
@@ -26,16 +50,5 @@ btnGameStart.addEventListener( 'click', function() {
             console.log('hai cliccato il numero' , + index + 1);
     
         })
-
     }
-})
-
-
-// * FUNCTIONS * \\
-function createNewSquare() {
-    const square = document.createElement('div');
-
-    square.classList.add('square');
-
-    return square;
 }
